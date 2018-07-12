a8c377e05feb5170643fd4e48c30bb2542a3a6e4
zane <811974803@qq.com>
2018-07-12 22:02:15 +0800

    disable scroll bar

--- init.el
+++ init.el
@@ -30,6 +30,7 @@
 
 (tool-bar-mode -1)
 (menu-bar-mode -1)
+(scroll-bar-mode -1)
 
 (global-nlinum-mode t)            ;; global nlinum mode
 (setq inhibit-startup-message t)  ;; disable startup message
