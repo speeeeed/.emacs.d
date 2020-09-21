753f1cab2c024cb9055d144800f63b8ebe981426
zane <811974803@qq.com>
2020-09-21 17:29:21 +0800

    tsinghua source

--- elisp/init-elpa.el
+++ elisp/init-elpa.el
@@ -3,9 +3,8 @@
 (require 'package)
 (setq package-enable-at-startup nil)
 (setq package-archives
-      '(("gnu" . "http://elpa.emacs-china.org/gnu/")
-        ("melpa-stable" . "http://elpa.emacs-china.org/melpa-stable/")
-        ("melpa" . "http://elpa.emacs-china.org/melpa/")))
+      '(("gnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+        ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
