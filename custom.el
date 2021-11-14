fc32ceb1b38c78272105d857d532822947ddf2cb
zun.wang <wangzun827@outlook.com>
2021-11-14 13:02:26 +0800

    optimize my emacs config

---
+++ custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("2925ed246fb757da0e8784ecf03b9523bccd8b7996464e587b081037e0e98001" default))
+ '(package-selected-packages
+   '(smartparens neotree posframe company which-key counsel-projectile counsel-gtags counsel ivy evil-collection evil-surround evil-leader evil undo-tree dts-mode markdown-mode use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- init.el
+++ init.el
@@ -13,18 +13,16 @@
 ;; Adjust garbage collection thresholds during startup, and thereafter
 ;;----------------------------------------------------------------------------
 (let ((normal-gc-cons-threshold (* 20 1024 1024))
-      (init-gc-cons-threshold (* 256 1024 1024)))
+      (init-gc-cons-threshold (* 128 1024 1024)))
   (setq gc-cons-threshold init-gc-cons-threshold)
   (add-hook 'emacs-startup-hook
             (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
 
-(setq custom-file "~/.emacs.d/custom.el")
+(setq custom-file (locate-user-emacs-file "custom.el"))
 (load custom-file)
 
 ;; init load path
-(add-to-list 'load-path "~/.emacs.d/elisp")
-(add-to-list 'load-path "~/.emacs.d/elisp/lang")
-;; (add-to-list 'load-path "~/.emacs.d/elisp/emacs-ccls")
+(add-to-list 'load-path "~/.emacs.d/lisp")
 
 ;; init-elpa should be first
 (require 'init-elpa)
@@ -32,7 +30,6 @@
 (require 'init-basic)
 (require 'init-evil)
 (require 'init-ivy)
-(require 'init-which-key)
 (require 'init-completion)
 (require 'init-neotree)
 ;; (require 'init-magit)
@@ -40,7 +37,3 @@
 ;; program language
 (require 'init-cc)
 (require 'init-python)
-
-;; (require 'init-lsp)
-;; (require 'init-yasnippet)
-;; (require 'init-flycheck)
